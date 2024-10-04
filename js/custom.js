df677e38bced41f26022a36fdffc61ecece93113
banyee19 <kunpengdu43@gmail.com>
2024-10-04 15:03:54 +0800

    Site updated: 2024-10-04 15:03:53

---
+++ js/custom.js
@@ -0,0 +1,4 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const subtitle = document.getElementById('subtitle');
+    subtitle.classList.add('visible'); // 页面加载后添加 visible 类
+});
