476dedb5f8e2e8f39652e857dd6a3828ca0a5554
banyee19 <kunpengdu43@gmail.com>
2025-06-27 12:02:52 +0800

    Site updated: 2025-06-27 12:02:51

--- js/sign.js
+++ js/sign.js
@@ -1,8 +1,4 @@
 const  navbarBrand  =  document.querySelector('.container a');
-/* 
-navbarBrand.innerHTML = `
-  <object type="image/svg+xml" data="/img/banyee's Blog.svg"></object>
-`; */
 
 /* <object> 标签会阻止点击事件传递到父元素。这是因为 <object> 内部的内容（SVG）会"吸收"点击事件。使用 pointer-events: none 可以解决这个问题 */
 
